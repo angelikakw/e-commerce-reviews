109ec5fb4e74bf37d4fd445c6fc3f2284fc1b6ba
angelikakwiatek <eg.angelika@gmail.com>
2021-10-04 21:20:21 +0200

    Pie chart

--- dash_app.py
+++ dash_app.py
@@ -2,6 +2,8 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 import plotly.express as px
+from dash.dependencies import Input, Output
+
 from eda import calculate_basic_statistics
 
 
@@ -12,11 +14,32 @@ def show_dashboard(data):
 
     text_style = dict(color='#444', fontFamily='sans-serif', fontWeight=300)
     plotly_figure = px.histogram(data, x="Age", title='Number of reviews per Age:')
+    rating_pie = px.pie(data, values=data['Rating'].value_counts().values, names=data['Rating'].value_counts().index,
+                        title='Rating')
+    class_name_nr_reviews = px.histogram(data, x="Class Name", title='Number of reviews per Class Name:')
 
     app.layout = html.Div([
         html.H2('Product Reviews', style=text_style),
         html.P('Number of analyzed reviews: ' + str(nr_of_all_reviews), style=text_style),
         dcc.Graph(id='plot1', figure=plotly_figure),
+        dcc.Dropdown(
+            id='names',
+            value=data['Class Name'].value_counts().index,
+            options=[{'value': x, 'label': x}
+                     for x in set(data['Class Name'])],
+            clearable=False
+        ),
+        dcc.Graph(id='plot2', figure=rating_pie),
+        dcc.Graph(id='plot3', figure=class_name_nr_reviews)
     ])
-    app.server.run()
 
+    @app.callback(
+        Output("plot2", "figure"),
+        Input("names", "value"))
+    def generate_chart(names):
+        fig = px.pie(data, values=data[data['Class Name'] == names]['Rating'].value_counts().values,
+                     names=data[data['Class Name'] == names]['Rating'].value_counts().index,
+                     title='Rating')
+        return fig
+
+    app.server.run(debug=True)
