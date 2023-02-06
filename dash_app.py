b385d7a0321bf94392592333ba74756ce09f5a71
angelikakw <eg.angelika@gmail.com>
2023-02-06 21:18:23 +0100

    Bar chart - test

--- dash_app.py
+++ dash_app.py
@@ -1,6 +1,6 @@
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html
+from dash import dcc
 import plotly.express as px
 from dash.dependencies import Input, Output
 
@@ -10,18 +10,21 @@ from eda import calculate_basic_statistics
 def show_dashboard(data):
     app = dash.Dash('Product Reviews')
 
-    nr_of_all_reviews = calculate_basic_statistics(data)
+    nr_of_all_reviews, nr_of_all_clothes = calculate_basic_statistics(data)
 
     text_style = dict(color='#444', fontFamily='sans-serif', fontWeight=300)
     plotly_figure = px.histogram(data, x="Age", title='Number of reviews per Age:')
     rating_pie = px.pie(data, values=data['Rating'].value_counts().values, names=data['Rating'].value_counts().index,
                         title='Rating')
+
     class_name_nr_reviews = px.histogram(data, x="Class Name", title='Number of reviews per Class Name:')
 
     app.layout = html.Div([
         html.H2('Product Reviews', style=text_style),
         html.P('Number of analyzed reviews: ' + str(nr_of_all_reviews), style=text_style),
-        dcc.Graph(id='plot1', figure=plotly_figure),
+        html.P('Number of rating clothes: ' + str(nr_of_all_clothes), style=text_style),
+        dcc.Graph(id='plot1', figure=class_name_nr_reviews),
+        dcc.Graph(id='plot2', figure=plotly_figure),
         dcc.Dropdown(
             id='names',
             value=data['Class Name'].value_counts().index,
@@ -29,8 +32,7 @@ def show_dashboard(data):
                      for x in set(data['Class Name'])],
             clearable=False
         ),
-        dcc.Graph(id='plot2', figure=rating_pie),
-        dcc.Graph(id='plot3', figure=class_name_nr_reviews)
+        dcc.Graph(id='plot3', figure=rating_pie)
     ])
 
     @app.callback(
--- eda.py
+++ eda.py
@@ -1,3 +1,8 @@
 def calculate_basic_statistics(data):
     nr_of_all_reviews = len(data.index)
-    return nr_of_all_reviews
+    nr_of_all_clothes = len(set(data['Clothing ID']))
+    return nr_of_all_reviews, nr_of_all_clothes
+
+
+
+
